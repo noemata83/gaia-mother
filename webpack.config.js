12bd8ac09f0f2126b651421a9431079c8e4b5cdb
Tucker McKinney <noemata83@gmail.com>
2018-10-02 15:26:44 -0400

    A reset: forking the Underscores theme again

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+  entry: ["@babel/polyfill", "./assets/js/app.js"],
+  output: {
+    path: path.resolve(__dirname, "assets/js"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+              presets: [
+                ['@babel/preset-env', {
+                  "modules": false,
+                  "targets": {
+                    "browsers": ["> 1%", "last 2 versions", "ie >= 11"]
+                    }
+                }], '@babel/preset-react'],
+              babelrc: false
+          },
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: "imports-loader?define=>false"
+      }
+    ]
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      proxy: 'http://localhost',
+      files: [
+        '**/*.php',
+        '**/*.css'
+      ],
+      reloadDelay: 0
+    })
+  ],
+  mode: 'development'
+}
