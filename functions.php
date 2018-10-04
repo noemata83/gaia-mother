0f6838b424e95afddc4986793a1cc39d8b8d11f7
Tucker McKinney <noemata83@gmail.com>
2018-10-04 11:54:47 -0400

    Added main content styling to fit below fixed header; removed sidebar from page.php, fixed FontAwesome (I hope)

--- functions.php
+++ functions.php
@@ -121,11 +121,12 @@ add_action( 'widgets_init', 'gaia_mother_widgets_init' );
  */
 function gaia_mother_scripts() {
 	wp_enqueue_style( 'gaia-mother-style', get_stylesheet_uri() );
+	wp_enqueue_style('FontAwesome', 'https://use.fontawesome.com/releases/v5.0.7/css/all.css');
+
 
 	wp_enqueue_script( 'gaia-mother-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'gaia-mother-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-	wp_enqueue_script( 'app scripts', get_template_directory_uri() . '/assets/js/bundle.js',  array(), microtime(), true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
