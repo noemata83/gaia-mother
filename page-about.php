b1b9eed4d5d11b056b668284776855db51063de5
Tucker McKinney <noemata83@gmail.com>
2018-10-09 14:49:12 -0400

    Built out mother about page

---
+++ page-about.php
@@ -0,0 +1,38 @@
+<?php
+get_header();
+?>
+<main id="about">
+    <div class="hero">
+        <div class="hero__text">
+            <p class="hero__lead">
+                Gaia.<br/>
+                We protect what matters.
+            </p>
+            <p class="hero__copy">
+                Few things are more important than our planet. The name Gaia (pronounced GUY-YA) comes from the gnostic term for Mother Earth, symbolic of our unyielding approach to developing safe and effective products for people, pets, and property.
+            </p>
+        </div>
+    </div>
+    <section class="sustainability">
+        <h2>A proven track record in sustainability</h2>
+        <p>
+            Our belief in the importance of sustainable products is why we create. SafePaw was developed in 1987 out of necessity. Nothing in the market met our standards for safety, so we created the product we could trust. Our work continues with new products that uphold our commitment to sustainability.
+        </p>
+    </section>
+    <div class="graphic">&nbsp;</div>
+    <section class="mission">
+        <div class="mission__text">
+            <h2>Our Mission and Vision</h2>
+            <p>
+                Our mission is simple: replace harmful products with safe options. We hope to one day be the most trusted manufacturer of environmentally safe products around the world.
+            </p>    
+        </div>
+    </section>
+    <section class="values">
+        <h2>Company Values</h2>
+        <p>
+            Gaia is committed to creating sustainable products. We value truth, sustainability, and hard work. Our team members share this passion and work each day to put people, pets, and property first.
+        </p>
+    </section>
+
+<?php get_footer();?>
