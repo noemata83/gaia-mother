05156981b88b1c93ecc6b18d6ecaed0e2a4a27fe
Tucker McKinney <noemata83@gmail.com>
2018-10-15 12:08:24 -0400

    Added page sidebar

--- functions.php
+++ functions.php
@@ -127,16 +127,25 @@ function gaia_mother_widgets_init() {
 		'after_widget'  => '</aside>',
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
-) );
+	) );
 	
-register_sidebar( array(
+	register_sidebar( array(
 		'name'          => __( 'Shop Sidebar', 'gaia-mother' ),
 		'id'            => 'shop',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
-) );
+	) );
+
+	register_sidebar( array(
+		'name'          => __( 'Page Sidebar', 'gaia-mother' ),
+		'id'            => 'page',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
 }
 add_action( 'widgets_init', 'gaia_mother_widgets_init' );
 
